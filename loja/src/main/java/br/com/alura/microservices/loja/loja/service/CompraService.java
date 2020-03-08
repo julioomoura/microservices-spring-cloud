1227101a6994a59392b236bd8495bf5c19c6fe4d
Júlio Moura <juliomourajom@gmail.com>
2020-03-08 03:33:39 -0300

    :sparkles: Implement eureka server in order to allow two microservices exchange data and information

--- loja/src/main/java/br/com/alura/microservices/loja/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservices/loja/loja/service/CompraService.java
@@ -2,6 +2,7 @@ package br.com.alura.microservices.loja.loja.service;
 
 import br.com.alura.microservices.loja.loja.dto.CompraDTO;
 import br.com.alura.microservices.loja.loja.dto.InfoFornecedorDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,9 +10,12 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompraService {
+
+    @Autowired
+    private RestTemplate client;
+
     public void realizaCompra(CompraDTO compra) {
-        RestTemplate client = new RestTemplate();
-        ResponseEntity<InfoFornecedorDTO> exchange = client.exchange("http://localhost:8081/info/" + compra.getEndereco().getEstado(),
+        ResponseEntity<InfoFornecedorDTO> exchange = client.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(),
                 HttpMethod.GET, null, InfoFornecedorDTO.class);
 
         System.out.println(exchange.getBody().getEndereco());
